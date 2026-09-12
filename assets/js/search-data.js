6a5a7398dca24a8915897bb76d9aa8c4d123ceac
chenhajaj <chenhajaj@gmail.com>
2026-09-12 14:52:53 +0000

    Deploying to gh-pages from @ chenhajaj/chenhajaj.github.io@585ca615a0bc9c6bb0128d6990ea08dbd77b6fda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-research-notes",
-          title: "Research Notes",
-          description: "Short, practical explanations of research from the Hajaj Lab.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/insights/";
-          },
         },{id: "nav-code-amp-software",
           title: "Code &amp; Software",
           description: "",
